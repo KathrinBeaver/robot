9a3d1c1758681be28fc34a4220905bd83a760c1b
KathrinBeaver <kathrin.beaver@mail.ru>
2024-02-18 17:54:39 +0300

    Remove unused imports

--- src/main/java/ru/mai/pvk/robot/RobotApplication.java
+++ src/main/java/ru/mai/pvk/robot/RobotApplication.java
@@ -2,8 +2,6 @@ package ru.mai.pvk.robot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import ru.mai.pvk.robot.property.RobotProperties;
 
 
 @SpringBootApplication
