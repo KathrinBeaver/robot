4fb524f3e458d8dd05513ebe8e008e2144cb1de3
Your Name <you@example.com>
2024-04-08 15:51:11 +0300

    add spring security initial commit

---
+++ src/main/java/ru/mai/pvk/robot/securingweb/security/controller/AuthController.java
@@ -0,0 +1,36 @@
+package ru.mai.pvk.robot.securingweb.security.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mai.pvk.robot.securingweb.security.domain.dto.JwtAuthenticationResponse;
+import ru.mai.pvk.robot.securingweb.security.domain.dto.SignInRequest;
+import ru.mai.pvk.robot.securingweb.security.domain.dto.SignUpRequest;
+import ru.mai.pvk.robot.securingweb.security.service.AuthenticationService;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Аутентификация")
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @Operation(summary = "Регистрация пользователя")
+    @PostMapping("/sign-up")
+    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        return authenticationService.signUp(request);
+    }
+
+    @Operation(summary = "Авторизация пользователя")
+    @PostMapping("/sign-in")
+    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return authenticationService.signIn(request);
+    }
+}
+
---
+++ src/main/java/ru/mai/pvk/robot/securingweb/security/domain/model/Role.java
@@ -0,0 +1,6 @@
+package ru.mai.pvk.robot.securingweb.security.domain.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
