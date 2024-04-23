caf93db5b8a56cd75d9a9466a7d7c1092b3420ed
Your Name <you@example.com>
2024-04-23 17:26:41 +0300

    fix some fake log issues

--- src/main/java/ru/mai/pvk/robot/service/impl/IssueCheckerServiceImpl.java
+++ src/main/java/ru/mai/pvk/robot/service/impl/IssueCheckerServiceImpl.java
@@ -6,6 +6,7 @@ import ru.mai.pvk.robot.model.dto.*;
 import ru.mai.pvk.robot.property.RobotProperties;
 import ru.mai.pvk.robot.service.IssueCheckerService;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -62,15 +63,30 @@ public class IssueCheckerServiceImpl implements IssueCheckerService {
         String address = robotProperties.getUrl();
         int startLog = (new Random()).nextInt(300);
 
-        for (int i = startLog; i < startLog + 20; i++) {
-            if (i < idStart) continue;
-            result.add(LogDto.of(i, wrapToHref(String.valueOf(i), address) + " passed"));
+        result.add(LogDto.of(idStart + 1, "LogId = " + idStart + ". Started at " + LocalDateTime.now().toLocalTime()));
+
+        for (int i = idStart + 1; i < idStart + 20; i++) {
+            if (i % 2 == 0) {
+                result.add(LogDto.of(i, wrapToHref(String.valueOf(900000 + i), address) + wrapToBold(" passed")));
+            } else {
+                result.add(LogDto.of(i, wrapToHref(String.valueOf(800000 + i), address) + wrapToRed(" failed")));
+            }
+
         }
+        result.add(LogDto.of(idStart + 20 + 1, "LogId = " + (idStart + 21) + ". Finished at " + LocalDateTime.now().toLocalTime()));
 
         return result;
     }
 
     private String wrapToHref(String issueId, String address) {
-        return "<a href='" + address + "issues/" + issueId + "'>" + issueId + "</a>";
+        return "<a href='" + address + "/issues/" + issueId + "'>" + issueId + "</a>";
+    }
+
+    private String wrapToBold(String str) {
+        return "<b>" + str + "</b>";
+    }
+
+    private String wrapToRed(String str) {
+        return "<span><font color=\"red\">" + str + "</font></span>";
     }
 }
